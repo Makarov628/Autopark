864de38c15e9de6e860178224271678f7c0ded36
Makarov628 <frostmakarov@gmail.com>
2025-06-01 17:38:24 +0500

    Task 6 - Enterprise get by id with included entities

--- UseCases/Enterprise/Queries/GetById/GetByIdEnterpriseQuery.cs
+++ UseCases/Enterprise/Queries/GetById/GetByIdEnterpriseQuery.cs
@@ -1,3 +1,4 @@
+using Autopark.Domain.BrandModel.Enums;
 using LanguageExt;
 using MediatR;
 
@@ -8,5 +9,49 @@ public record GetByIdEnterpriseQuery(int Id) : IRequest<Fin<EnterpriseResponse>>
 public record EnterpriseResponse(
     int Id,
     string Name,
-    string Address
+    string Address,
+    Vehicle[] Vehicles,
+    Driver[] Drivers
+);
+
+public record BrandModel(
+    int Id,
+    string BrandName,
+    string ModelName,
+    TransportType TransportType,
+    FuelType FuelType,
+    uint SeatsNumber,
+    uint MaximumLoadCapacityInKillograms
+);
+
+public record Vehicle(
+    int Id,
+    string Name,
+    decimal Price,
+    double MileageInKilometers,
+    string Color,
+    string RegistrationNumber,
+    BrandModel BrandModel,
+    Driver? ActiveDriver,
+    Driver[] AttachedDrivers
+);
+
+public record VehicleForDriver(
+    int Id,
+    string Name,
+    decimal Price,
+    double MileageInKilometers,
+    string Color,
+    string RegistrationNumber,
+    BrandModel BrandModel,
+    int? ActiveDriverId
+);
+
+public record Driver(
+    int Id,
+    string FirstName,
+    string LastName,
+    DateTime DateOfBirth,
+    decimal Salary,
+    VehicleForDriver? AttachedVehicle = null
 );
--- UseCases/Enterprise/Queries/GetById/GetByIdEnterpriseQueryHandler.cs
+++ UseCases/Enterprise/Queries/GetById/GetByIdEnterpriseQueryHandler.cs
@@ -23,10 +23,85 @@ internal class GetByIdEnterpriseQueryHandler : IRequestHandler<GetByIdEnterprise
         if (enterprise is null)
             return Error.New($"Enterprise not found with id: {request.Id}");
 
+        var vehicles = await _dbContext.Vehicles
+            .AsNoTracking()
+            .Where(v => v.EnterpriseId == enterpriseId)
+            .ToListAsync(cancellationToken);
+
+        var drivers = await _dbContext.Drivers
+            .AsNoTracking()
+            .Where(d => d.EnterpriseId == enterpriseId)
+            .ToListAsync(cancellationToken);
+
+        var brandModelIds = vehicles.Select(v => v.BrandModelId).ToList();
+        var brandModels = await _dbContext.BrandModels
+            .AsNoTracking()
+            .Where(bm => brandModelIds.Contains(bm.Id))
+            .ToListAsync(cancellationToken);
+
         return new EnterpriseResponse(
             enterprise.Id.Value,
             enterprise.Name.Value,
-            enterprise.Address
-        );
+            enterprise.Address,
+            vehicles.ConvertAll(vehicle => new Vehicle(
+                vehicle.Id.Value,
+                vehicle.Name.Value,
+                vehicle.Price.Value,
+                vehicle.MileageInKilometers.ValueInKilometers,
+                vehicle.Color.Value,
+                vehicle.RegistrationNumber.Value,
+                brandModels.Where(bm => bm.Id == vehicle.BrandModelId).Select(bm => new BrandModel(
+                    bm.Id.Value,
+                    bm.BrandName,
+                    bm.ModelName,
+                    bm.TransportType,
+                    bm.FuelType,
+                    bm.SeatsNumber,
+                    bm.MaximumLoadCapacityInKillograms
+                )).First(),
+                vehicle.ActiveDriverId != null
+                    ? drivers.Select(d => new Driver(
+                        d.Id.Value,
+                        d.FirstName.Value,
+                        d.LastName.Value,
+                        d.DateOfBirth,
+                        d.Salary
+                    )).FirstOrDefault(d => d.Id == vehicle.ActiveDriverId.Value)
+                    : null,
+                drivers.Where(d => d.VehicleId is not null && d.VehicleId == vehicle.Id).Select(d => new Driver(
+                    d.Id.Value,
+                    d.FirstName.Value,
+                    d.LastName.Value,
+                    d.DateOfBirth,
+                    d.Salary
+                )).ToArray()
+            )).ToArray(),
+            drivers.Select(d => new Driver(
+                d.Id.Value,
+                d.FirstName.Value,
+                d.LastName.Value,
+                d.DateOfBirth,
+                d.Salary,
+                d.VehicleId is not null ? vehicles.Where(v => v.Id == d.VehicleId).Select(v => new VehicleForDriver(
+                    v.Id.Value,
+                    v.Name.Value,
+                    v.Price.Value,
+                    v.MileageInKilometers.ValueInKilometers,
+                    v.Color.Value,
+                    v.RegistrationNumber.Value,
+                    brandModels.Where(bm => bm.Id == v.BrandModelId).Select(bm => new BrandModel(
+                        bm.Id.Value,
+                        bm.BrandName,
+                        bm.ModelName,
+                        bm.TransportType,
+                        bm.FuelType,
+                        bm.SeatsNumber,
+                        bm.MaximumLoadCapacityInKillograms
+                    )).First(),
+                    v.ActiveDriverId?.Value)
+                ).First()
+                : null
+            )
+            ).ToArray());
     }
 }
